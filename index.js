7259b58528e2fdaa116dff2b17eafea74e959729
debugeverything-template <95109497+debugeverything-template@users.noreply.github.com>
2022-12-14 18:53:35 -0300

    Commit message

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter yep
-What task is this?!?!?</h1>`;
+appDiv.innerHTML = `<h1>Debug Everything</h1>`;
